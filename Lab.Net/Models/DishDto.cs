bc3fdb9b2a33aac1938e45db8424776dae3a9410
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-10-14 01:19:09 +0700

    Updated and filled DishDto

--- Lab.Net/Models/DishDto.cs
+++ Lab.Net/Models/DishDto.cs
@@ -5,5 +5,26 @@ namespace Lab.Net.Models
     public class DishDto
 
     {
+        [Required(ErrorMessage = "The field is required.")]
+
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public bool Vegetarian { get; set; }
+        public double Rating { get; set; }
+        public enum Category
+        {
+            Wok,
+            Pizza,
+            Soup,
+            Dessert,
+            Drink
+        }
+
     }
 }
