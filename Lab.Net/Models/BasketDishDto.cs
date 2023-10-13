44d52fca5f562ea3592cd745c45da55a431a3585
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-10-13 18:22:00 +0700

    Created a new classes: LoginCredentials, OrderCreateDto, OrderDto

--- Lab.Net/Models/BasketDishDto.cs
+++ Lab.Net/Models/BasketDishDto.cs
@@ -1,6 +1,19 @@
-﻿namespace Lab.Net.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab.Net.Models
 {
     public class BasketDishDto
     {
+        [Required(ErrorMessage = "The field is required.")]
+        [RegularExpression(@"^.*\S.*$", ErrorMessage = "The field cannot be empty or contain only white spaces.")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        public string Image { get; set;}
     }
 }
--- Lab.Net/Models/DishPagedListDto.cs
+++ Lab.Net/Models/DishPagedListDto.cs
@@ -1,6 +1,18 @@
 ﻿namespace Lab.Net.Models
 {
-    public class DishPagedListDto
-    {
-    }
+        public class PageInfoModule
+        {
+            public int size { get; set; }
+            public int count { get; set; }
+            public int current { get; set; }
+
+        }
+
+        public class DishPagedListDto
+        {
+            public PageInfoModule pagination { get; set; }
+            public List<DishDto> dishes { get; set; }
+
+        }
+    
 }
