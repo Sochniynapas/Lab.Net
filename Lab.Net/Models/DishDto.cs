409ce3b4ab9d1502f8f6b24dcf9a000de5c00093
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-10-13 16:20:42 +0700

    Created a dish class

--- Lab.Net/Models/DishDto.cs
+++ Lab.Net/Models/DishDto.cs
@@ -1,6 +1,31 @@
-﻿namespace Lab.Net.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab.Net.Models
 {
     public class DishDto
+
     {
+        [Required(ErrorMessage = "The field is required.")]
+        [RegularExpression(@"^.*\S.*$", ErrorMessage = "The field cannot be empty or contain only white spaces.")]
+        public int Id { get; set; }
+
+        [MinLength(1)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public bool Vegetarian { get; set; }
+        public double Rating { get; set; }  
+        public enum Category
+        {
+            Wok,
+            Pizza,
+            Soup,
+            Dessert,
+            Drink
+        }
+
     }
 }
